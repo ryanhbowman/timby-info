025e37c084eb6ecb668347acaa9f4cdfe942a056
Ryan Bowman <ryan@circle.co.ke>
2014-11-24 00:49:57 +0000

    added dynamic menu, icons, feature page header and submenu

--- js/app.js
+++ js/app.js
@@ -3,10 +3,19 @@ $( document ).ready(function() {
       $('.mobile-menu').toggleClass('active-menu');
       $('#menu_control').toggleClass('active');
   });
+  $features = $('.page-header__features');
+  if ($features.length > 0){
+    $featuresHeight = $features.innerHeight();
+  }
 });
 $(window).scroll(function(event) {
   $scrolled = $(window).scrollTop();
-
+  if ($features.length > 0 && $scrolled > $featuresHeight-140){
+    $('.features-menu-scroll').addClass('is-active');
+  }
+  else {
+    $('.features-menu-scroll').removeClass('is-active');
+  }
   // if scrolled past main header, show the little header
   if ($scrolled > 140) {
     $('.header').addClass('is-active');
